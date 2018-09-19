a6599bc241c433704a510b0a7c1c00d13e32b802
rifkegribenes <rifkegribenes@gmail.com>
2018-09-19 12:17:11 -0700

    built display

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app" id="drum-machine">
+        <div className="drum-pad-wrap">
+          <div className="display" id="display" />
+          <button className="drum-pad" id="biff">Q
+            <audio src="" className="clip" id="Q" />
+          </button>
+          <button className="drum-pad" id="boom">W
+            <audio src="" className="clip" id="W" />
+          </button>
+          <button className="drum-pad" id="bang">E
+            <audio src="" className="clip" id="E" />
+          </button>
+          <button className="drum-pad" id="crash">A
+            <audio src="" className="clip" id="A" />
+          </button>
+          <button className="drum-pad" id="bada">S
+            <audio src="" className="clip" id="S" />
+          </button>
+          <button className="drum-pad" id="pow">D
+            <audio src="" className="clip" id="D" />
+          </button>
+          <button className="drum-pad" id="ping">Z
+            <audio src="" className="clip" id="Z" />
+          </button>
+          <button className="drum-pad" id="ratatat">X
+            <audio src="" className="clip" id="X" />
+          </button>
+          <button className="drum-pad" id="tap">C
+            <audio src="" className="clip" id="C" />
+          </button>
+        </div>
       </div>
     );
   }
