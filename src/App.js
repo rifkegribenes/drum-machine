77405df63fec55ba8f80132960a0315004dec5a8
rifkegribenes <rifkegribenes@gmail.com>
2018-09-19 18:06:54 -0700

    cleaned up render

--- src/App.js
+++ src/App.js
@@ -89,60 +89,15 @@ class App extends Component {
       <div className="app" id="drum-machine">
         <div className="drum-pad-wrap">
           <div className="display" id="display">{this.state.name}</div>
-          <button
-            className="drum-pad"
-            id="bang"
-            onClick={(e) => this.handleClick(e)}>Q
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/bang.mp3' className="clip" id="Q" />
-          </button>
-          <button
-            className="drum-pad"
-            id="bawm"
-            onClick={(e) => this.handleClick(e)}>W
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/bawm.mp3' className="clip" id="W" />
-          </button>
-          <button
-            className="drum-pad"
-            id="chack"
-            onClick={(e) => this.handleClick(e)}>E
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/chack.mp3' className="clip" id="E" />
-          </button>
-          <button
-            className="drum-pad"
-            id="cheee"
-            onClick={(e) => this.handleClick(e)}>A
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/cheee.mp3' className="clip" id="A" />
-          </button>
-          <button
-            className="drum-pad"
-            id="chick"
-            onClick={(e) => this.handleClick(e)}>S
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/chick.mp3' className="clip" id="S" />
-          </button>
-          <button
-            className="drum-pad"
-            id="crash"
-            onClick={(e) => this.handleClick(e)}>D
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/crash.mp3' className="clip" id="D" />
-          </button>
-          <button
-            className="drum-pad"
-            id="pip"
-            onClick={(e) => this.handleClick(e)}>Z
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/pip.mp3' className="clip" id="Z" />
-          </button>
-          <button
-            className="drum-pad"
-            id="ratatat"
-            onClick={(e) => this.handleClick(e)}>X
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/ratatat.mp3' className="clip" id="X" />
-          </button>
-          <button
-            className="drum-pad"
-            id="snap"
-            onClick={(e) => this.handleClick(e)}>C
-            <audio src='https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/snap.mp3' className="clip" id="C" />
-          </button>
+          {sounds.map(sound => (
+            <button
+              className="drum-pad"
+              key={sound.id}
+              id={sound.name}
+              onClick={(e) => this.handleClick(e)}>{sound.id}
+              <audio src={`https://raw.githubusercontent.com/rifkegribenes/drum-machine/master/src/audio/${sound.name}.mp3`} className="clip" id={sound.id} />
+            </button>
+            ))}
         </div>
       </div>
     );
