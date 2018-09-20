fa20256964976ab6567ad2ec302d7efc1b5cadac
rifkegribenes <rifkegribenes@gmail.com>
2018-09-19 17:20:23 -0700

    rename audio files

--- src/App.js
+++ src/App.js
@@ -1,67 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-
   const sounds = [
     {
-      keyCode:
-      id: "Q"
-      name: "bang"
+      keyCode: 81,
+      id: "Q",
+      name: "bang",
       src: './audio/Bang.mp3'
     },
     {
-      keyCode:
-      id: "W"
-      name: "bawm"
+      keyCode: 87,
+      id: "W",
+      name: "bawm",
       src: './audio/Bawm.mp3'
     },
     {
-      keyCode:
-      id: "E"
-      name: "chack"
+      keyCode: 69,
+      id: "E",
+      name: "chack",
       src: './audio/Chack.mp3'
     },
     {
-      keyCode:
-      id: "A"
-      name: "cheee"
+      keyCode: 65,
+      id: "A",
+      name: "cheee",
       src: './audio/Cheee.mp3'
     },
     {
-      keyCode:
-      id: "S"
-      name: "chick"
+      keyCode: 83,
+      id: "S",
+      name: "chick",
       src: './audio/Chick.mp3'
     },
     {
-      keyCode:
-      id: "D"
-      name: "crash"
+      keyCode: 68,
+      id: "D",
+      name: "crash",
       src: './audio/Crash.mp3'
     },
     {
-      keyCode:
-      id: "Z"
-      name: "pip"
+      keyCode: 90,
+      id: "Z",
+      name: "pip",
       src: './audio/Pip.mp3'
     },
     {
-      keyCode:
-      id: "X"
-      name: "ratatat"
+      keyCode: 88,
+      id: "X",
+      name: "ratatat",
       src: './audio/Ratatat.mp3'
     },
     {
-      keyCode:
-      id: "C"
-      name: "snap"
+      keyCode: 67,
+      id: "C",
+      name: "snap",
       src: './audio/Snap.mp3'
     }
-  ]
+  ];
+
+class App extends Component {
 
   playSound = (e) => {
-    const id = e.target.id;
+    const name = e.target.id;
+    const soundObj = sounds.filter((sound => sound.name === name));
+    const audioId = soundObj[0].id;
+    console.log(document.getElementById(audioId).src);
+    document.getElementById(audioId).play();
 
   }
 
@@ -74,55 +78,55 @@ class App extends Component {
             className="drum-pad"
             id="bang"
             onClick={(e) => this.playSound(e)}>Q
-            <audio src={bang} className="clip" id="Q" />
+            <audio src='./audio/bang.mp3' className="clip" id="Q" />
           </button>
           <button
             className="drum-pad"
             id="bawm"
             onClick={(e) => this.playSound(e)}>W
-            <audio src={bawm} className="clip" id="W" />
+            <audio src='./audio/bawm.mp3' className="clip" id="W" />
           </button>
           <button
             className="drum-pad"
             id="chack"
             onClick={(e) => this.playSound(e)}>E
-            <audio src={chack} className="clip" id="E" />
+            <audio src='./audio/chack.mp3' className="clip" id="E" />
           </button>
           <button
             className="drum-pad"
             id="cheee"
             onClick={(e) => this.playSound(e)}>A
-            <audio src={cheee} className="clip" id="A" />
+            <audio src='./audio/cheee.mp3' className="clip" id="A" />
           </button>
           <button
             className="drum-pad"
             id="chick"
             onClick={(e) => this.playSound(e)}>S
-            <audio src={chick} className="clip" id="S" />
+            <audio src='./audio/chick.mp3' className="clip" id="S" />
           </button>
           <button
             className="drum-pad"
             id="crash"
             onClick={(e) => this.playSound(e)}>D
-            <audio src={crash} className="clip" id="D" />
+            <audio src='./audio/crash.mp3' className="clip" id="D" />
           </button>
           <button
             className="drum-pad"
             id="pip"
             onClick={(e) => this.playSound(e)}>Z
-            <audio src={pip} className="clip" id="Z" />
+            <audio src='./audio/pip.mp3' className="clip" id="Z" />
           </button>
           <button
             className="drum-pad"
             id="ratatat"
             onClick={(e) => this.playSound(e)}>X
-            <audio src={ratatat} className="clip" id="X" />
+            <audio src='./audio/ratatat.mp3' className="clip" id="X" />
           </button>
           <button
             className="drum-pad"
             id="snap"
             onClick={(e) => this.playSound(e)}>C
-            <audio src={snap} className="clip" id="C" />
+            <audio src='./audio/snap.mp3' className="clip" id="C" />
           </button>
         </div>
       </div>
